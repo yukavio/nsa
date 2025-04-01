27741444616156637d3fa9b60c7fd970e407acc7
yukavio <kavioyu@gmail.om>
2025-04-01 13:14:05 +0000

    fix dk dv

--- nsa/triton_attention.py
+++ nsa/triton_attention.py
@@ -176,9 +176,8 @@ def _attn_bwd_preprocess(O, DO,  #
     off_h = tl.program_id(2)
     off_n = tl.arange(0, HEAD_DIM)
     # load
-    o = tl.load(O + off_z * HEAD_DIM * N_CTX * H + off_m[:, None] * HEAD_DIM * H + off_n[None, :])
-    do = tl.load(O + off_z * HEAD_DIM * N_CTX * H + off_m[:, None] * HEAD_DIM * H + off_n[None, :])
-    #do = tl.load(DO + off_z * HEAD_DIM * N_CTX * H + off_m[:, None] * HEAD_DIM * H + off_n[None, :]).to(tl.float32)
+    o = tl.load(O + off_z * HEAD_DIM * N_CTX * H + off_h * HEAD_DIM + off_m[:, None] * HEAD_DIM * H + off_n[None, :])
+    do = tl.load(DO + off_z * HEAD_DIM * N_CTX * H + off_h * HEAD_DIM + off_m[:, None] * HEAD_DIM * H + off_n[None, :])
     delta = tl.sum(o * do, axis=1)
     # write-back
     tl.store(Delta + off_z * N_CTX * H + off_h + off_m * H, delta)
@@ -190,7 +189,7 @@ def _attn_bwd_dkdv(dk, dv,  #
                    Q, k, v, sm_scale,  #
                    DO,  #
                    M, D,  #
-                   # shared by Q/K/V/DO.
+                   # shared by Q/DO.
                    stride_tok, stride_d,  #
                    H, N_CTX, BLOCK_M1: tl.constexpr,  #
                    BLOCK_N1: tl.constexpr,  #
@@ -211,7 +210,7 @@ def _attn_bwd_dkdv(dk, dv,  #
         qT = tl.load(qT_ptrs)
         # Load m before computing qk to reduce pipeline stall.
         offs_m = curr_m + tl.arange(0, BLOCK_M1)
-        m = tl.load(M + offs_m)
+        m = tl.load(M + offs_m*H)
         qkT = tl.dot(k, qT)
         pT = tl.math.exp2(qkT - m[None, :])
         # Autoregressive masking.
@@ -224,7 +223,7 @@ def _attn_bwd_dkdv(dk, dv,  #
         ppT = ppT.to(dk.dtype)
         dv += tl.dot(ppT, do)
         # D (= delta) is pre-divided by ds_scale.
-        Di = tl.load(D + offs_m)
+        Di = tl.load(D + offs_m*H)
         # Compute dP and dS.
         dpT = tl.dot(v.to(do.dtype), tl.trans(do)).to(tl.float32)
         dsT = pT * (dpT - Di[None, :])
@@ -323,98 +322,83 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     offs_k = tl.arange(0, HEAD_DIM)
 
     start_n = pid * BLOCK_N1
-    start_m = start_n
+    start_m = 0
 
-    MASK_BLOCK_M1: tl.constexpr = BLOCK_M1 // BLK_SLICE_FACTOR
     offs_n = start_n + tl.arange(0, BLOCK_N1)
 
     dv = tl.zeros([BLOCK_N1, HEAD_DIM], dtype=tl.float32)
     dk = tl.zeros([BLOCK_N1, HEAD_DIM], dtype=tl.float32)
 
     # load K and V: they stay in SRAM throughout the inner loop.
-    k = tl.load(K + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d)
-    v = tl.load(V + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d)
+    k = tl.load(K + offs_n[:, None] * stride_ktok + offs_k[None, :] * stride_kd)
+    v = tl.load(V + offs_n[:, None] * stride_ktok + offs_k[None, :] * stride_kd)
 
-    num_steps = BLOCK_N1 // MASK_BLOCK_M1
+    num_steps = Q_CTX  // BLOCK_M1
 
-    # dk, dv = _attn_bwd_dkdv(dk, dv,  #
-    #                         Q, k, v, sm_scale,  #
-    #                         DO,  #
-    #                         M, D,  #
-    #                         stride_tok, stride_d,  #
-    #                         H, Q_CTX,  #
-    #                         MASK_BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
-    #                         start_n, start_m, num_steps,  #
-    #                         MASK=True  #
-    #                         )
+    # Compute dK and dV for non-masked blocks.
+    dk, dv = _attn_bwd_dkdv(  #
+        dk, dv,  #
+        Q, k, v, sm_scale,  #
+        DO,  #
+        M, D,  #
+        stride_tok, stride_d,  #
+        H, KV_CTX,  #
+        BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
+        start_n, start_m, num_steps,  #
+        MASK=False  #
+    )
 
-    # start_m += num_steps * MASK_BLOCK_M1
-    # num_steps = (KV_CTX - start_m) // BLOCK_M1
+    dv_ptrs = DV + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
+    tl.store(dv_ptrs, dv)
 
-    # # Compute dK and dV for non-masked blocks.
-    # dk, dv = _attn_bwd_dkdv(  #
-    #     dk, dv,  #
-    #     Q, k, v, sm_scale,  #
-    #     DO,  #
-    #     M, D,  #
-    #     stride_tok, stride_d,  #
-    #     H, KV_CTX,  #
-    #     BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
-    #     start_n, start_m, num_steps,  #
-    #     MASK=False  #
-    # )
+    # Write back dK.
+    dk *= sm_scale
+    dk_ptrs = DK + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
+    tl.store(dk_ptrs, dk)
 
-    # dv_ptrs = DV + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
-    # tl.store(dv_ptrs, dv)
+    # # THIS BLOCK DOES DQ:
+    # start_m = pid * BLOCK_M2
+    # end_n = start_m + BLOCK_M2
 
-    # # Write back dK.
-    # dk *= sm_scale
-    # dk_ptrs = DK + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
-    # tl.store(dk_ptrs, dk)
+    # MASK_BLOCK_N2: tl.constexpr = BLOCK_N2 // BLK_SLICE_FACTOR
+    # offs_m = start_m + tl.arange(0, BLOCK_M2)
 
-    # THIS BLOCK DOES DQ:
-    start_m = pid * BLOCK_M2
-    end_n = start_m + BLOCK_M2
+    # q = tl.load(Q + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d)
+    # dq = tl.zeros([BLOCK_M2, HEAD_DIM], dtype=tl.float32)
+    # do = tl.load(DO + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d)
 
-    MASK_BLOCK_N2: tl.constexpr = BLOCK_N2 // BLK_SLICE_FACTOR
-    offs_m = start_m + tl.arange(0, BLOCK_M2)
+    # m = tl.load(M + offs_m*H)
+    # m = m[:, None]
 
-    q = tl.load(Q + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d)
-    dq = tl.zeros([BLOCK_M2, HEAD_DIM], dtype=tl.float32)
-    do = tl.load(DO + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d)
-
-    m = tl.load(M + offs_m*H)
-    m = m[:, None]
-
-    # Compute dQ for masked (diagonal) blocks.
-    # NOTE: This code scans each row of QK^T backward (from right to left,
-    # but inside each call to _attn_bwd_dq, from left to right), but that's
-    # not due to anything important.  I just wanted to reuse the loop
-    # structure for dK & dV above as much as possible.
-    num_steps = BLOCK_M2 // MASK_BLOCK_N2
-    dq = _attn_bwd_dq(dq, q, K, V,  #
-                      do, m, D,  #
-                      stride_tok, stride_d,  #
-                      H, Q_CTX,  #
-                      BLOCK_M2, MASK_BLOCK_N2, HEAD_DIM,  #
-                      start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,  #
-                      MASK=True  #
-                      )
-    end_n -= num_steps * MASK_BLOCK_N2
-    # stage 2
-    num_steps = end_n // BLOCK_N2
-    dq = _attn_bwd_dq(dq, q, K, V,  #
-                      do, m, D,  #
-                      stride_tok, stride_d,  #
-                      H, Q_CTX,  #
-                      BLOCK_M2, BLOCK_N2, HEAD_DIM,  #
-                      start_m, end_n - num_steps * BLOCK_N2, num_steps,  #
-                      MASK=False  #
-                      )
-    # Write back dQ.
-    dq_ptrs = DQ + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
-    dq *= LN2
-    tl.store(dq_ptrs, dq)
+    # # Compute dQ for masked (diagonal) blocks.
+    # # NOTE: This code scans each row of QK^T backward (from right to left,
+    # # but inside each call to _attn_bwd_dq, from left to right), but that's
+    # # not due to anything important.  I just wanted to reuse the loop
+    # # structure for dK & dV above as much as possible.
+    # num_steps = BLOCK_M2 // MASK_BLOCK_N2
+    # dq = _attn_bwd_dq(dq, q, K, V,  #
+    #                   do, m, D,  #
+    #                   stride_tok, stride_d,  #
+    #                   H, Q_CTX,  #
+    #                   BLOCK_M2, MASK_BLOCK_N2, HEAD_DIM,  #
+    #                   start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,  #
+    #                   MASK=True  #
+    #                   )
+    # end_n -= num_steps * MASK_BLOCK_N2
+    # # stage 2
+    # num_steps = end_n // BLOCK_N2
+    # dq = _attn_bwd_dq(dq, q, K, V,  #
+    #                   do, m, D,  #
+    #                   stride_tok, stride_d,  #
+    #                   H, Q_CTX,  #
+    #                   BLOCK_M2, BLOCK_N2, HEAD_DIM,  #
+    #                   start_m, end_n - num_steps * BLOCK_N2, num_steps,  #
+    #                   MASK=False  #
+    #                   )
+    # # Write back dQ.
+    # dq_ptrs = DQ + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
+    # dq *= LN2
+    # tl.store(dq_ptrs, dq)
 
 
 class _attention(torch.autograd.Function):
@@ -465,6 +449,7 @@ class _attention(torch.autograd.Function):
     @staticmethod
     def backward(ctx, do, ds):
         q, k, v, o, M = ctx.saved_tensors
+        assert do.is_contiguous()
         assert q.stride() == o.stride() == do.stride()
         assert k.stride() == v.stride()
         dq = torch.empty_like(q)
@@ -482,7 +467,6 @@ class _attention(torch.autograd.Function):
         assert Q_CTX % PRE_BLOCK == 0
         pre_grid = (Q_CTX // PRE_BLOCK, BATCH, Q_HEAD)
         delta = torch.empty_like(M)
-
         _attn_bwd_preprocess[pre_grid](
             o, do,  #
             delta,  #
@@ -490,6 +474,8 @@ class _attention(torch.autograd.Function):
             BLOCK_M=PRE_BLOCK, HEAD_DIM=ctx.HEAD_DIM  #
         )
         grid = (Q_CTX // BLOCK_N1, BATCH, Q_HEAD)
+        print(delta.shape)
+        print(M.shape)
         _attn_bwd[grid](
             q, arg_k, v, ctx.sm_scale, do, dq, dk, dv,  #
             M, delta,  #
